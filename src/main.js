b1ea337350cb1a1cd17f8a6ecedf57d9796eee8a
ivettemalagon <ivettemayerly@hotmail.com>
2020-12-22 04:16:57 -0500

    update

--- src/main.js
+++ src/main.js
@@ -3,16 +3,10 @@
 import Vue from 'vue'
 import App from './App'
 import vueRouter from 'vue-router'
-
 import router from './router'
 Vue.use(vueRouter)
 
-
-
-
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
 new Vue({
   router,
   el: '#app',
--- src/router.js
+++ src/router.js
@@ -1,45 +1,45 @@
 import vueRouter from 'vue-router'
-
-
 import User from './components/User'
 import UserAuth from './components/UserAuth'
-import UserBalance from './components/UserBalance'
-import UserTransaction from './components/UserTransaction'
+import Inventory from './components/Inventory'
+import Search from './components/Search'
+import Modify from './components/Modify'
 import App from './App'
 
-
 const router = new vueRouter({
-        mode: 'history',
-        base: __dirname,
-        routes: [
-            {
-                path: '/',
-                name: "root",
-                component: App
-            },
-            {
-                path: '/user/:username',
-                name: "user",
-                component: User
-            },
-            {
-                path: '/user/auth',
-                name: "user_auth",
-                component: UserAuth
-            },
-            {
-                path: '/user/balance/:username',
-                name: "user_balance",
-                component: UserBalance
-            },
-            {
-                path: '/user/transaction/:username',
-                name: "user_transaction",
-                component: UserTransaction
-            },
-        ]
-    })
-
-
-
+    mode: 'history',
+    base: __dirname,
+    routes: [
+        {
+        path: '/',
+        name: "root",
+        component: App
+        },
+        {
+            path: '/user/:username',
+            name: "user",
+            component: User
+        },
+        {
+            path: '/user/auth',
+            name: "user_auth",
+            component: UserAuth
+        },
+        {
+        path: '/inventory',
+        name: "inventory",
+        component: Inventory
+        },
+        {
+        path: '/inventory/product/',
+        name: "buscar",
+        component: Search
+        },
+        {
+        path: '/inventory/product/modify/',
+        name: "modificar",
+        component: Modify
+        },
+    ]
+})
 export default router
